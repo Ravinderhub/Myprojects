af82751193535cb9918753f15650a7f68cef54ff
Ravinderhub <42468313+Ravinderhub@users.noreply.github.com>
2021-09-07 16:18:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "aws" {
-  region = var.aws_region
+  region  = "${var.region}"
+  profile = "${var.profile}"   
+  access_key=AKIA2NGXACR55TRNYD4K
+  secret_key=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
+}
 }
 
 #Create security group with firewall rules
